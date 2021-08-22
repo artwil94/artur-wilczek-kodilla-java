8d999203287afe7e606b12e0239d61934b804f11
Artur Wilczek <aw.wilczek@gmail.com>
2021-08-22 21:46:44 +0200

    Calculator-test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,40 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
 
     public static void main(String[] args) {
-        System.out.println("Moduł6. Wprowadzenie do testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+                if(result.equals("theForumUser")) {
+                    System.out.println("test OK");
+                }else{
+                    System.out.println("Error!");
+                }
+
+        System.out.println("Test - pierwszy test jednostkowy:");
+
+           Calculator calculator = new Calculator();
+
+
+                         int result1 = calculator.Add(5, 10);
+                         System.out.println(result1);
+
+                         int result2 = calculator.Subtract(5,10);
+                         System.out.println(result2);
+
+
+
+
+
+
+
+
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,16 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int Add(int a, int b){
+        return a + b;
+
+    }
+
+    public int  Subtract(int a, int b) {
+        return b - a;
+    }
+
+
+
+}
