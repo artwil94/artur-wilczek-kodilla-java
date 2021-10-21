340733589992b91454b234a1ef87571b494ed9a7
Artur Wilczek <aw.wilczek@gmail.com>
2021-10-21 11:37:13 +0200

    7.2 Stream

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 import com.kodilla.stream.lambda.ExpressionExecutor;
 import com.kodilla.stream.reference.FunctionalCalculator;
 
@@ -15,7 +16,11 @@ public class StreamMain {
         poemBeautifier.beautify("XYZ", (add)-> add + text + add);
         poemBeautifier.beautify(" ", (add)-> text.toUpperCase());
 
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
+
     }
+
 }
 
 
