834ef8863e23a4ea5e2402aa2c0a1941d450dcd9
Artur Wilczek <aw.wilczek@gmail.com>
2021-11-10 15:49:33 +0100

    7.5 GetAverage IntStream

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -0,0 +1,18 @@
+package com.kodilla.stream.array;
+
+import java.util.stream.IntStream;
+
+public interface ArrayOperations {
+
+
+    static double getAverage(int[] numbers) {
+                 IntStream.range(0, 21)
+                .forEach(System.out::println);
+
+                 IntStream.range(0,21)
+                         .average();
+
+
+        return 0;
+    }
+}
