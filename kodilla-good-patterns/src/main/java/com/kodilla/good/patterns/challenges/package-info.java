f1ceb1d0d330c6709e9c56c99ee1f35ff0fe23cb
Artur Wilczek <aw.wilczek@gmail.com>
2021-12-30 14:43:16 +0100

    13.1 Display movie titles

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/package-info.java
@@ -0,0 +1,1 @@
+package com.kodilla.good.patterns.challenges;
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/RunRun.java
@@ -0,0 +1,34 @@
+package com.kodilla.stream;
+
+
+
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.person.People;
+
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class RunRun {
+
+
+
+
+    public static void main(String[] args) {
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()  // [1]
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));                    // [2]
+
+        System.out.println(theResultStringOfBooks);
+    }
+}
+
+
