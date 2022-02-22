ee56c7325497511ea2d28c16e7ad624c74e104dd
Artur Wilczek <aw.wilczek@gmail.com>
2022-02-22 07:40:15 +0100

    14.1 ForumUserBeanTest

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -0,0 +1,30 @@
+package com.kodilla.spring.forum;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+public class ForumUserTestSuite {
+
+        @Test
+        void testGetUsername() {
+            //Given
+            ApplicationContext context = new AnnotationConfigApplicationContext("com.kodilla.spring");
+
+
+            //When
+           // String username =context.getBean(ForumUser.class).getUsername();
+
+            //Then
+            assertEquals("John Smith", context.getBean(ForumUser.class).getUsername());
+        }
+
+    }
+
+
